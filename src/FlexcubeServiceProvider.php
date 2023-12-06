8a7cc835e725af92249b325c4d0595530f05c049
noorfarooqy <noor@drongo.vip>
2023-12-06 16:55:38 +0300

    update the configs

--- src/FlexcubeServiceProvider.php
+++ src/FlexcubeServiceProvider.php
@@ -9,6 +9,10 @@ class FlexcubeServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        $this->publishes([
+            __DIR__ . '/../config/flexcube.php' => config_path('flexcube.php'),
+        ], 'flexcube-config');
+
     }
 
     public function register()
