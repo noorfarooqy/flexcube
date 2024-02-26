770f5885fcd3fef07a15f2475ec497d1c12f8d44
noorfarooqy <noor@drongo.vip>
2024-02-26 18:15:32 +0300

    updat eethe bank gateway

--- app/Services/CoreBankingContract.php
+++ app/Services/CoreBankingContract.php
@@ -24,7 +24,7 @@ interface CoreBankingContract
 
     public function ExchangeRate($from, $to, $branch = '000');
 
-    public function AccountBlockAmount($account, $branch, $amount, $hp_code, $ref, $expires_at = now()->addDay());
+    public function AccountBlockAmount($account, $branch, $amount, $hp_code, $ref, $expires_at = null);
     public function AccountUnblockAmount($ref);
     public function QueryAmountBlock($account_number, $block_no);
     public function GetAccountAmountBlocks($account);
